0f76af77420751cc97b60d93b5f345072b29c12e
AhmadFrazDEV <chahmad2004@gmail.com>
2023-11-29 02:20:52 +0500

    Update Existing User Balance
    Before, There was an issue that if user add balance first time it add it but when he again add balance it make another wallet.
    
    -- User can contain  only one wallet
    -- Balance will update if user has wallet

--- user_functions/addAmount.php
+++ user_functions/addAmount.php
@@ -13,7 +13,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Additional validation if needed
 
     // Check if the user already exists in the wallets table
-   
+    $checkUserSql = "SELECT * FROM wallets WHERE user_id = ?";
+    $checkUserStmt = $conn->prepare($checkUserSql);
+    $checkUserStmt->bind_param("i", $userData['user_id']);
+    $checkUserStmt->execute();
+    $result = $checkUserStmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // User exists, update their balance
+        $updateSql = "UPDATE wallets SET balance = balance + $amount WHERE user_id = {$userData['user_id']}";
+        $updateStmt = $conn->prepare($updateSql);
+        $updateStmt->bind_param("di", $amount, $userData['user_id']);
+        if ($updateStmt->execute()) {
+            header("Location: ../Wallet.php");
+            exit();
+        } else {
+            echo "Error updating balance: " . $updateStmt->error;
+        }
+        $updateStmt->close();
+    } else {
+        // User doesn't exist, insert a new row
+        $insertSql = "INSERT INTO wallets (user_id, balance, currency) VALUES ('{$userData['user_id']}', '$amount', '$currency')";
+        $insertStmt = $conn->prepare($insertSql);
+        $insertStmt->bind_param("ids", $userData['user_id'], $amount, $currency);
+        if ($insertStmt->execute()) {
+            header("Location: ../Wallet.php");
+            exit();
+        } else {
+            echo "Error inserting data: " . $insertStmt->error;
+        }
+        $insertStmt->close();
+    }
 
     // Close the statement and connection
     $checkUserStmt->close();
